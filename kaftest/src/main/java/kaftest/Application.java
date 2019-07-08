499edaeed6ce4a5820ce3a94e6edeeec8a74c9f3
graemerocher <graeme.rocher@gmail.com>
2019-07-08 09:22:18 +0200

    Kafka example working

--- kaftest/src/main/java/kaftest/Application.java
+++ kaftest/src/main/java/kaftest/Application.java
@@ -1,10 +1,28 @@
 package kaftest;
 
-import io.micronaut.runtime.Micronaut;
+import javax.inject.Singleton;
 
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.Micronaut;
+import io.micronaut.runtime.event.annotation.EventListener;
+import io.micronaut.scheduling.annotation.Scheduled;
+
+@Singleton
 public class Application {
+
+    private final BookProducer bookProducer;
+
+    Application(BookProducer bookProducer) {
+        this.bookProducer = bookProducer;
+    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class);
+    }
+
+    @Scheduled(initialDelay = "30s", fixedDelay = "5s")
+    void startup() {
+        System.out.println("Sending message");
+        bookProducer.send("Hello World");
     }
 }
--- kaftest/src/main/java/kaftest/BookProducer.java
+++ kaftest/src/main/java/kaftest/BookProducer.java
@@ -1,8 +1,11 @@
 package kaftest;
 
 import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.configuration.kafka.annotation.Topic;
 
 @KafkaClient
 public interface BookProducer {
 
+    @Topic("foo")
+    void send(String msg);
 }
